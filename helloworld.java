10a25134e395dd52ececfa34398c4192f0f9e2d3
VitalRanga <47557653+VitalRanga@users.noreply.github.com>
2019-06-08 20:36:34 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -5,6 +5,7 @@ class HelloWorld
     // Your program begins with a call to main(). 
     // Prints "Hello, World" to the terminal window. 
     public static void main(String args[]) 
+    https://github.com/VitalRanga/jenkinslab.git
     { 
         System.out.println("Hello, World"); 
     } 
